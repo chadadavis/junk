f991835546b2804c8270464336f88c495caf3d41
Chad Davis <chad.a.davis@gmail.com>
2011-09-06 16:49:04 +0200

    UTF demos

---
+++ perl/utf.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl
+
+use Modern::Perl;
+
+use utf8;
+
+my $var = 'schön';
+
+my $filename = shift || 'filename.txt';
+my $layer = ':encoding(UTF-8)';
+# my $layer = '';
+
+open my $fh, '>', $filename;
+
+# binmode select, ':encoding(UTF-8)';
+binmode $fh,    ':encoding(UTF-8)';
+
+print $fh $var, "\n";
