1a64c3be1532f4dd63cec35049ee5321e03459b3
Chad Davis <chad.a.davis@gmail.com>
2011-09-19 16:53:27 +0200

    Devel::Comments docs

--- perl/dbi-current_user.pl
+++ perl/dbi-current_user.pl
@@ -1,8 +1,13 @@
 #!/usr/bin/env perl
-use Modern::Perl;
+use strict;
 use DBI;
 my $dbh = DBI->connect('dbi:mysql:host=russelllab.org;database=trans_3_0;', '%');
 use Data::Dumper; 
 print Dumper $dbh->selectall_arrayref('select current_user;');
 
 print Dumper $dbh->selectall_arrayref('select * from trans limit 1;');
+
+
+# anonymous user
+my $local = DBI->connect('dbi:mysql:database=trans_3_0;host=russelllab.org','anonymous');
+
