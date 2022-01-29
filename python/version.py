4f292bcbc7c25152d69a41984af87ef4054a1cb7
Chad A. Davis <chad.a.davis@gmail.com>
2022-01-29 21:27:13 +0100

    junk

---
+++ python/version.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from pprint import pprint
+import re
+def pvars(_extra:dict=None):
+    """Also pass pp(vars()) from inside a def"""
+    _vars = { **globals(), **locals(), **(_extra if _extra else {}) }
+    pprint([ [k,_vars[k]] for k in _vars if re.match(r'[a-z]', k)])
+
+
+import sys
+
+print(sys.version_info)
