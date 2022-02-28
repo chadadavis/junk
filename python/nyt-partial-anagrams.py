d892e309780237246a1fc17c65c3ab07827d55ef
Chad A. Davis <chad.a.davis@gmail.com>
2022-02-28 12:52:42 +0100

    NYT spelling-bee: keep track of already-accepted (or rejected) words

--- python/nyt-partial-anagrams.py
+++ python/nyt-partial-anagrams.py
@@ -8,6 +8,10 @@
 import subprocess
 from optparse import OptionParser
 from wordfreq import zipf_frequency
+from operator import itemgetter
+import re
+import readline
+
 
 # DICT_FILE = '/usr/share/dict/british-english'
 # DICT_FILE = '/usr/share/dict/cracklib-small'
@@ -46,8 +50,41 @@ def is_complete_word(word, alphabet):
     return True
 
 
+def beep():
+    print("\a", end='', flush=True)
+
+
+scores = dict()
+def completer(text: str, state: int) -> str:
+    """Readline (TAB) autocompletion of remaining candidate words"""
+    global scores
+
+    completions = []
+    if not text:
+        return
+
+    # Completions via recent spellcheck suggestions (from last online fetch)
+    completions += [
+                    s for s in scores
+                    # Only complete words not yet processed
+                    if not scores[s]['status'] and s.startswith(text.casefold())
+                    ]
+
+    if state < len(completions):
+        return completions[state]
+
+    if state == 0:
+        # text doesn't match any possible completion
+        beep()
+
+
+readline.set_completer(completer)
+readline.parse_and_bind("tab: complete")
+
+
 file = open(DICT_FILE)
 count = 0
+
 for word in file:
     word = word.strip()
     if len(word) < MIN_LEN:
@@ -57,7 +94,7 @@ for word in file:
     if not is_valid_word(word, alphabet):
         continue
     count += 1
-    complete = '*' if is_complete_word(word, alphabet) else ' '
+    complete = '*' if is_complete_word(word, alphabet) else ''
 
     # This puts two scores on a (roughly) 1-100 scale
     l = 10 * len(word)
@@ -66,11 +103,60 @@ for word in file:
     # And (equally weighed) average them
     difficulty = (freq + l) / 2
 
-    # This sed command strips off the first two header lines from the output
-    # And the ' ... || true' makes this actually *not* check the return code for success,
-    # since no definition is also fine.
-    definition = subprocess.check_output(f'dict -f -d wn {word} 2>/dev/null|sed -e1,2d|xargs 2>/dev/null || true', shell=True)
-    definition = definition.decode('utf-8')
-    definition = definition.strip()
-    definition = definition[:200]
-    print(f"{count:3d} {freq:3.0f} {l:3.0f} {difficulty:3.0f} {complete:1s} {word:20s} {definition:s}")
+    scores[word] = {
+        'word': word,
+        'freq': freq,
+        'l': l,
+        'difficulty': difficulty,
+        'status': complete,
+    }
+
+
+while True:
+    # Headings
+    print(f"{'#':>2} {'len':>4} {'freq':>4} {'comb':>4} {'*':>1} {'word'}")
+    # Remaining words, sorted, by status+difficulty
+    scores_sorted = scores.values()
+    scores_sorted = sorted(scores_sorted, key=itemgetter('difficulty'), reverse=False)
+    scores_sorted = sorted(scores_sorted, key=itemgetter('status'),     reverse=False)
+
+    for i, s in enumerate(scores_sorted):
+        print(f"{i:2d} {s['l']:4.0f} {s['freq']:4.0f} {s['difficulty']:4.0f} {s['status']:>1s} {s['word']:20s}")
+    print()
+
+    # All words covered?
+    if len( [ k for k in scores if not scores[k]['status'] ]    ) == 0:
+        exit()
+
+    # Mark words as accepted / rejected, while any left (without status)
+    try:
+        cmd = input("cmd [? + -]: ")
+    except:
+        print()
+        exit()
+
+    match = re.match('\s*([?+-]?)\s*(.+)\s*', cmd)
+    if not match:
+        beep()
+        continue
+    op, word = match.groups()
+    if word not in scores:
+        beep()
+        continue
+
+    # The default operator is to mark he word as already accepted
+    op = op or '+'
+    if op in ('-', '+'):
+        scores[word]['status'] = op
+
+    # Lookup definition of a word
+    if op == '?':
+        # This sed command strips off the first two header lines from the output
+        # And the ' ... || true' makes this actually *not* check the return code for success,
+        # since no definition is also fine.
+        definition = subprocess.check_output(f'dict -f -d wn {word} 2>/dev/null|sed -e1,2d|xargs 2>/dev/null || true', shell=True)
+        definition = definition.decode('utf-8')
+        definition = definition.strip()
+        definition = definition[:200]
+        print("\n", definition, "\n")
+
