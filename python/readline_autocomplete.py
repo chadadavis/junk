4e883680edf0226c3edb6a49fa5de4ba092ef44a
Chad A. Davis <chad.a.davis@gmail.com>
2022-09-18 10:30:59 +0200

    Some examples on using readline completer

--- python/readline_autocomplete.py
+++ python/readline_autocomplete.py
@@ -9,17 +9,51 @@ def pvars(_extra:dict=None):
 
 import readline
 
+# For accent-insensitive completion
+import unidecode
+
 opts = ['angela@domain.com', 'mikey@smack.com', 'michael@domain.com', 'david@test.com']
 
-def completer(text, state):
-    options = [x for x in opts if x.startswith(text)]
-    try:
-        return options[state]
-    except IndexError:
-        return None
+def completer(text: str, state: int) -> str:
+    completions = []
+
+    # Allow global completion of the whole dictionary before any text?
+    # if not text:
+    #     return
+
+    # Unidecode allows accent-insensitive autocomplete
+    ud = unidecode.unidecode
+    text = ud(text)
+
+    # # Completions via readline history
+    # for i in range(1, readline.get_current_history_length() + 1):
+    #     i = readline.get_history_item(i)
+    #     if ud(i).casefold().startswith(text.casefold()):
+    #         completions += [ i ]
+
+    # Autocomplete via prefix search
+    for i in opts:
+        if ud(i).casefold().startswith(text.casefold()):
+            completions += [ i ]
+
+    if state < len(completions):
+        try:
+            return completions[state]
+        except IndexError:
+            return None
+
+
+    if state == 0:
+        # text doesn't match any possible completion
+        # eg beep()
+        ...
+
 
 readline.set_completer(completer)
 readline.parse_and_bind("TAB: complete")
+
+# Allow to complete multi-token terms (with spaces)
+readline.set_completer_delims('')
 
 while 1:
     a = input("> ")
