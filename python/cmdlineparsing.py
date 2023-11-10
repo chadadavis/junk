e3b773595e318e2116b871f15809b60a7304a2ba
Chad A Davis <chad.a.davis@gmail.com>
2023-11-10 15:57:33 +0100

    .

--- python/cmdlineparsing.py
+++ python/cmdlineparsing.py
@@ -2,58 +2,58 @@
 
 import sys
 import re
-from optparse import OptionParser
-
-# TODO optparse deprecated, switch to argparse
+import argparse
 
 # parse command line params
-parser = OptionParser()
-parser.add_option("-p", "--plot", dest="plot_filename",
-				  help="print GNUplot TM prediction for a single protein to FILE",\
-				  metavar="FILE")
-parser.add_option("-d", "--dist", dest="dist_filename",
-				  help="print GNUplot TM distribution for a series of protein to FILE",\
-				  metavar="FILE")
-parser.add_option("--debug",
-				  action="store_false", dest="debug", default=False,
-				  help="print debugging information to stdout")
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-p",
+    "--plot",
+    dest="plot_filename",
+    help="print GNUplot TM prediction for a single protein to FILE",
+    metavar="FILE",
+)
+parser.add_argument(
+    "-d",
+    "--dist",
+    dest="dist_filename",
+    help="print GNUplot TM distribution for a series of protein to FILE",
+    metavar="FILE",
+)
+parser.add_argument(
+    "--debug",
+    action="store_true",
+    # dest="debug",
+    # default=False,
+    help="print debugging information to stdout",
+)
 
-(options, args) = parser.parse_args()
-mydict = {1: 1, 2: 2}
+args = parser.parse_args()
+my_dict = {1: 1, 2: 2}
 
-print("Options: ", options, " Args: ", args, "\n")
+print(" Args: ", args, "\n")
 
-
-	
-if options.plot_filename:
-	print("yes")
-
-#if options["plot_filename"]:
-#	print options["plot_filename"]
-
+if args.plot_filename:
+    print("yes")
 
 sequence = ""
 #s = sys.stdin.readline()
 #while s :
-#	s = sys.stdin.readline()
-#	while s and not re.match('^>', s) :
-#		sequence += s
-#		s = sys.stdin.readline()
-#	print sequence, "\n\n"
-#	sequence = ""
+#    s = sys.stdin.readline()
+#    while s and not re.match('^>', s) :
+#        sequence += s
+#        s = sys.stdin.readline()
+#    print sequence, "\n\n"
+#    sequence = ""
 
 
 something = {}
-if not something.has_key(0) :
-	something[0] = 1
+if not 0 in something :
+    something[0] = 1
 else:
-	something[0] += 1
-	
+    something[0] += 1
+
+# Raises KeyError if key 0 doesn't already exist
 #something[0] += 1
 something[1] = 1
-
 print(something.keys())
-
-
-		
-	
